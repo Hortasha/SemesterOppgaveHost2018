236faecac491760c7772345abbc418bf28d1df1f
hortasha <eidemartin_303@hotmail.com>
2018-11-14 15:42:10 +0100

    socket.io bullshittery - connection works

--- index.js
+++ index.js
@@ -15,23 +15,74 @@ app.use(express.static('public'));
 var io = socket(server);
 
 //Game Variables
-var games = [];
+var lobbys = [];
 
-
+//Connected
 io.on('connection', function(socket) {
+//Connection data
+  var room = "";
+  var player = "";
   console.log('connected', socket.id);
 
+//Disconnected
   socket.on('disconnect', function(){
+    if(room !== "") {
+      for(var i = 0; i < room.players.length; i++) {
+        if(room.players[i].id === socket.id) {
+          room.players.splice(i, 1);
+        }
+      }
+      io.to(room.name).emit('enter lobby', room);
+    }
     console.log('user disconnected', socket.id);
   });
 
-  socket.on('createGame', function(data) {
+//Creating game
+  socket.on('create game', function(data) {
     var result = createGame(data);
-    if(result) {
-      socket.emit('createGameSuccess', data);
-
+    if(result !== false) {
+      player = new Player(socket.id);
+      lobbys[result].players.push(player);
+      socket.emit('enter lobby', lobbys[result]);
+      socket.join(data.name);
+      room = lobbys[result];
     } else {
-      socket.emit('createGameFail', data);
+      socket.emit('create game failed', data);
+    }
+  });
+
+  //Requesting lobbys
+  socket.on('request lobbys', function() {
+    socket.emit('recive lobbys', lobbys);
+  });
+
+  //Join Lobby
+  socket.on('join lobby', function(data) {
+    for(var i = 0; i < lobbys.length; i++) {
+      if (lobbys[i].name === data.name && lobbys[i].maxPlayers > lobbys[i].players.length) {
+        player = new Player(socket.id);
+        lobbys[i].players.push(player);
+        socket.join(data.name);
+        room = lobbys[i];
+        io.to(data.name).emit('enter lobby', lobbys[i]);
+      }
+    }
+  });
+
+//Start the game and remove from lobby
+  socket.on('start', function() {
+    io.to(room.name).emit('character selection', room);
+    for(var i = 0; i < lobbys.length; i++) {
+      if(lobbys[i] === room) {
+        lobbys.splice(i, 1);
+      }
+    }
+    room.board = new Board(30);
+    if(room.players[0] === player) {
+      socket.emit('choose character', {
+        room,
+        player
+      });
     }
   });
 });
@@ -45,13 +96,17 @@ function createGame(data) {
   if (data.name === "") {
     return false;
   }
-  for(var i = 0; i < games.length; i++) {
-    if(data.name === games[i].name) {
-      return false;
+  if(lobbys.length > 0) {
+    for(var i = 0; i < lobbys.length; i++) {
+      if(data.name === lobbys[i].name) {
+        return false;
+      }
     }
   }
-  games.push(new Game(data.name, data.type));
-  return true;
+
+  var newGame = new Game(data.name, data.type);
+  lobbys.push(newGame);
+  return lobbys.length-1;
 }
 
 /**
@@ -62,9 +117,29 @@ function createGame(data) {
    constructor(name, type) {
      this.name = name;
      this.type = type;
-     this.lobby = true;
      this.board = {};
      this.players = [];
      this.dices = 1;
+     this.maxPlayers = 2;
    }
  }
+
+ class Player {
+   constructor(id) {
+     this.id = id;
+     this.character = {};
+     this.tile = 1;
+   }
+ }
+
+class Board {
+  constructor(tiles) {
+    this.tiles = tiles;
+  }
+}
+
+class Tile {
+  constructor() {
+
+  }
+}
--- public/code.js
+++ public/code.js
@@ -1,19 +1,94 @@
-var socket = io();
-
+//Variables
 //Elements
 var standardStart = document.getElementById("standardStart");
 var gameName = document.getElementById("gameName");
+var serverList = document.getElementById("serverList");
+var body = document.getElementById("body");
+var lobbyButtons = document.getElementsByClassName("lobby");
 
-//Variables
+//Socket
+var socket = io();
+
 standardStart.addEventListener("click", function() {
-  socket.emit('createGame', {
+  socket.emit('create game', {
     name: gameName.value,
     type: "default"
   });
 });
 
-//listen
+function start() {
+  socket.emit('start');
+}
 
-socket.on('createGameFail', function(data) {
+//listen
+socket.on('create game failed', function(data) {
   console.log("Failed to create game with the name " + data.name + ". Try another name.");
 });
+
+socket.on('enter lobby', function(data) {
+  refreshLobby(data);
+});
+
+socket.on('recive lobbys', function(data) {
+  serverList.innerHTML = "";
+  for (var i = 0; i < data.length; i++) {
+    serverList.innerHTML += '<button type="button" onclick="enterLobby(\'' + data[i].name + '\')">' + data[i].name + '</button>';
+  }
+});
+
+socket.on('character selection', function(data) {
+  body.innerHTML = "<h1>Player 1 chooses character</h1>";
+  getJSON()
+    .then(function (result) {
+      console.log(result);
+      body.innerHTML += "<p>" + result[0].aliases[0] + "</p>";
+    });
+});
+
+socket.on('choose character', function(data) {
+  for(var i = 0; i < data.room.players.length; i++) {
+    if(data.room.players[i] === data.player) {
+      //Player choosing character
+    }
+  }
+});
+
+//functions
+function requestLobbys() {
+  socket.emit('request lobbys');
+}
+
+function refreshLobby(data) {
+  body.innerHTML = "<h1>Players in lobby</h1>";
+  for (var i = 0; i < data.players.length; i++) {
+    body.innerHTML += "<p>" + data.players[i].id + "</p>";
+  }
+  body.innerHTML += '<button type="button" onclick="start()">Start</button>';
+}
+
+function enterLobby(lobbyName) {
+  socket.emit('join lobby', {
+    name: lobbyName
+  });
+}
+
+function getJSON() {
+ return new Promise(
+
+ resolve => {
+   fetch('https://anapioficeandfire.com/api/characters')
+     .then(function(response) {
+       return response.json();
+     })
+     .then(function(myJson) {
+       resolve(myJson);
+     });
+ },
+
+ reject => {
+   reject("Rip API");
+ });
+}
+
+//Calls
+requestLobbys();
